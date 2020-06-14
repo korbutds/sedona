7d037f27ea2ff69922399cee45d7c8a678a95b70
korbutds <sys@hohou.ru>
2020-06-14 08:42:14 +0300

    Оживляем интерактивные элементы

--- js/form.js
+++ js/form.js
@@ -1,6 +1,9 @@
 var formButton = document.querySelector('.hotel-btn');
 var bookingForm = document.querySelector('form');
 
+  bookingForm.classList.remove('form-open');
+  bookingForm.classList.add('form-close');
+
 formButton.addEventListener('click', function(evt) {
     evt.preventDefault();
     bookingForm.classList.toggle('form-close');
